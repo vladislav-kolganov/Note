9f170ddbd5b4438922f314f6c613c2a7d49ad0e0
golubchik2002 <golubchik2002@gmail.com>
2024-12-13 23:23:34 +0300

    Добавил тесты

---
+++ Note.Tests/Configurations/MapperConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Note.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note.Tests.Configurations
+{
+    public static class MapperConfigurations
+    {
+        public static IMapper GetMapperConfiguration()
+        {
+            var mockMapper = new MapperConfiguration(cfg => 
+            {
+                cfg.AddProfile(new ReportMapping());
+            });
+
+            return mockMapper.CreateMapper();
+        }
+    }
+}
