feb864fae6dddbc78a4fa383bbed8118f3fa9a7b
Vladislav Kolganov <v.kolganov@hybrid.ai>
2025-12-08 10:52:42 +0300

    код стайл поправил

--- Note.Domain/Dto/AiChatDto/ChatRequestDto.cs
+++ Note.Domain/Dto/AiChatDto/ChatRequestDto.cs
@@ -4,6 +4,6 @@
 /// Дто запроса к сервису.
 /// </summary>
 /// <param name="UserId">Id пользователя.</param>
-/// <param name="UserPrompt">Промт для ИИ пользователя</param>
+/// <param name="UserPrompt">Промт для ИИ пользователя.</param>
 /// <param name="SystemPrompt">Системный промт.</param>
 public record ChatRequestDto(long UserId, string UserPrompt);
